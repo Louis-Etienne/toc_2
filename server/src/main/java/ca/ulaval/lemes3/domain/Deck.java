7d96ba9012bebb2cbf2ddbd04999b2f61c0a916c
Sofia Sekkak <sosek1@ulaval.ca>
2026-06-30 15:27:20 -0400

    test: tested the basic behavior of Deck

--- server/src/main/java/ca/ulaval/lemes3/domain/Deck.java
+++ server/src/main/java/ca/ulaval/lemes3/domain/Deck.java
@@ -33,4 +33,8 @@ public class Deck {
         return cards.remove(0);
     }
 
+    public int size() {
+        return cards.size();
+    }
+
 }
--- server/src/test/java/ca/ulaval/lemes3/domain/DeckTest.java
+++ server/src/test/java/ca/ulaval/lemes3/domain/DeckTest.java
@@ -1,7 +1,55 @@
 package ca.ulaval.lemes3.domain;
 
+import org.junit.jupiter.api.Test;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 class DeckTest {
+
+    @Test
+    void givenNewDeck_whenCreated_thenContains52Cards() {
+        Deck deck = new Deck();
+
+        assertEquals(52, deck.size());
+    }
+
+    @Test
+    void givenDeck_whenDrawingCard_thenReturnsCard() {
+        Deck deck = new Deck();
+
+        Card card = deck.drawTopCard();
+
+        assertNotNull(card);
+    }
+
+    @Test
+    void givenDeck_whenDrawingCard_thenDeckSizeDecrease() {
+        Deck deck = new Deck();
+
+        deck.drawTopCard();
+
+        assertEquals(51, deck.size());
+    }
+
+    @Test
+    void givenDeck_whenReseting_thenSizeIs52() {
+        Deck deck = new Deck();
+
+        Card card = deck.drawTopCard();
+        deck.resetDeck();
+
+        assertEquals(52, deck.size());
+    }
+
+    @Test
+    void givenEmptyDeck_whenDrawingCard_thenThrowException() {
+        Deck deck = new Deck();
+
+        for (int i = 0; i < 52; i++) {
+            deck.drawTopCard();
+        }
+
+        assertThrows(IllegalStateException.class, deck::drawTopCard);
+    }
 
 }
